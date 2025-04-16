9eacd0fb2aa04868cdc5b2099269bdac5592f8b5
obed-smart <codingwithsmart@gmail.com>
2025-04-16 15:06:17 +0100

    Fix: added hash function convert string to bytes32, and also some Modifiers and event

--- school-registery/register.sol
+++ school-registery/register.sol
@@ -5,27 +5,53 @@ contract SchoolRegister {
     struct Register {
         string name;
         uint256 id;
-        uint256 class;
+        string class;
+        bytes32 hashClass;
     }
+    address private owner;
     mapping(address => Register[]) private schoolrecord;
 
-    function registerStudent(
-        string calldata _name,
-        uint256 _id,
-        uint256 _class
-    ) public {
+    constructor() {
+        owner = msg.sender;
+    }
+
+    event RegisteredStudent(uint256 id, string name, string class);
+
+    modifier onlySchool() {
+        require(msg.sender == owner, "only school can use the function");
+        _;
+    }
+
+    uint256 studentIdCount = 1;
+
+    function hashString(string memory _str) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_str));
+    }
+
+    function registerStudent(string calldata _name, string calldata _class)
+        public
+        onlySchool
+    {
         // require(msg.sender == address(0), "only school can register new studen");
 
         Register memory studentinfo = Register({
             name: _name,
-            id: _id,
-            class: _class
+            id: studentIdCount,
+            class: _class,
+            hashClass: hashString(_class)
         });
 
+        studentIdCount++;
         schoolrecord[msg.sender].push(studentinfo);
+        emit RegisteredStudent(studentIdCount, _name, _class);
     }
 
-    function getAllStudentRecord() public view returns (Register[] memory) {
+    function getAllStudentRecord()
+        public
+        view
+        onlySchool
+        returns (Register[] memory)
+    {
         return schoolrecord[msg.sender];
     }
 
@@ -43,31 +69,43 @@ contract SchoolRegister {
         revert("no student found");
     }
 
-    function getStudentByClass(uint256 _class)
-        public view 
+    /*
+     * get the student record by class
+     * @param(_class)- the class of each related students
+     * loop through the records return the number of students in a class
+     * create a new fixed size array for the students with the return number
+     * for each students that match the class store them in the new arrya
+     */
+
+    function getStudentByClass(string calldata _class)
+        public
+        view
         returns (Register[] memory)
     {
-        Register[] memory records = schoolrecord[msg.sender];
+        Register[] memory records = schoolrecord[msg.sender]; // asign the register to records
 
-        uint256 newRecordCount = 0;
+        uint256 newRecordCount = 0; //  create a variable to store the number of students in a class
 
+        // loop through the register to count student
         for (uint256 i = 0; i < records.length; i++) {
-            if (records[i].class == _class) {
-                newRecordCount++;
+            if (records[i].hashClass == hashString(_class)) {
+                newRecordCount++; // increase the number of students in a class if the condition is true
             }
         }
 
-        Register[] memory matchedStudent = new Register[](newRecordCount);
+        Register[] memory matchedStudent = new Register[](newRecordCount); //  create a fixed array for the number of students in a class
 
-        uint256 index = 0;
+        uint256 index = 0; // create a new variable to store student in a class
+
+        // loop to find the student in a class
 
         for (uint256 j = 0; j < records.length; j++) {
-            if (records[j].class == _class) {
-                matchedStudent[index] = records[j];
-                index++;
+            if (records[j].hashClass == hashString(_class)) {
+                matchedStudent[index] = records[j]; // store the student in a class to new fixed array
+                index++; // increase the index to store the student in a class
             }
         }
 
-        return matchedStudent;
+        return matchedStudent; // return the array with students in a specific class
     }
 }
