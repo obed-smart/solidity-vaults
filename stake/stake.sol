b4e476e666d144311f6c41b569118055efb6c16b
obed-smart <codingwithsmart@gmail.com>
2025-05-02 21:13:39 +0100

    updated the file

--- stake/stake.sol
+++ stake/stake.sol
@@ -14,11 +14,12 @@ contract FitechStaking {
         uint256 duration;
         uint256 stakedTime;
         uint256 unStakeTime;
-        bool claimed;
+        bool active;
     }
 
     mapping(address => User) private userStake;
-    mapping(address => uint) private stakedAmountByUser;
+    mapping(address => uint256) private stakedAmountByUser;
+    mapping(address => uint256) private rewardAmountByUser;
 
     event Staked(address indexed _user, uint256 _amount, uint256 stakedTime);
     event unStaked(
@@ -30,19 +31,52 @@ contract FitechStaking {
     constructor(address _rewardToken) {
         rewardsToken = IERC20(_rewardToken);
         owner = msg.sender;
-        rewardsToken.transferFrom(msg.sender, address(this), 1000 * 10 ** 18);
     }
 
-    function stake(uint256 _amount) public payable {
-        require(_amount != 0, "you can not stake zero ethereum");
+
+    function stake(uint256 _durationInDay) external payable {
+        require(msg.value != 0, "Staking amount can not be zero");
+        require(_durationInDay != 0, "diration can not be zero");
+        require(msg.sender != address(0), "invalid address");
+
+        uint256 _amount = msg.value;
+        require(
+            !userStake[msg.sender].active,
+            "you have an active stake please try updatingstake instead"
+        );
+
+        userStake[msg.sender] = User({
+            amount: _amount,
+            duration: (_durationInDay * 1 days),
+            stakedTime: block.timestamp,
+            unStakeTime: block.timestamp + (_durationInDay * 1 days),
+            active: true
+        });
+
+        stakedAmountByUser[msg.sender] += _amount;
+        totalStaked += _amount;
+
+        emit Staked(msg.sender, _amount, block.timestamp);
+    }
+
+     function updatStaking() external payable {
+        require(msg.value != 0, "you can not stake zero ethereum");
+        require(msg.sender != address(0), "invalid address");
+
+        User storage user = userStake[msg.sender];
+
+        user.amount += msg.value;
+    }
+
+    function getUserTotalStakeAmount(address _user) public view  returns (uint) {
+
+uint user = rewardAmountByUser[_user];
+
+// require(, message);
+
+        return rewardAmountByUser[_user];
         
-     
     }
 
-
-
-    // function stake(uint256 _amount, uint256 _durationInDay) public payable  {
-    //     require(_amount != 0, "you can not stake zero ethereum");
-    //     // require(condition);
-    // }
+    function withdraw() external {}
 }
